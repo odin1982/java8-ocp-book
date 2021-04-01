deec89f73b1e731db9e330cc204cc9be22114d69
odin-huawei <odin.araujo.barragan@gmail.com>
2021-04-01 11:33:50 -0600

    avance

---
+++ src/main/java/org/ocp/pag032/bird/Toucan.java
@@ -0,0 +1,6 @@
+package org.ocp.pag032.bird;
+
+public class Toucan {
+	public static class Beak{}
+
+}
---
+++ src/main/java/org/ocp/pag032/bird/watcher/BirdWatcher.java
@@ -0,0 +1,9 @@
+package org.ocp.pag032.bird.watcher;
+
+//import org.ocp.pag032.bird.Toucan.Beak;
+//or
+import static org.ocp.pag032.bird.Toucan.Beak;
+
+public class BirdWatcher {
+	Beak beak;
+}
