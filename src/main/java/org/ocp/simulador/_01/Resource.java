3855cf9b61bc6ddcea7a8f4ea50404ef2272c456
odin-huawei <odin.araujo.barragan@gmail.com>
2021-04-28 00:34:31 -0500

    avance

--- src/main/java/org/ocp/simulador/_01/Resource.java
+++ src/main/java/org/ocp/simulador/_01/Resource.java
@@ -14,5 +14,20 @@ public class Resource {
 	boolean equals(Resource r) {
 		return(r != null && r.getData().equals(this.getData()));
 	}
+	
+	/*
+	public static void main(String[] args) {
+		Resource r1 = new Resource();
+		Resource r2 = new Resource();
+		System.out.println("Son iguales con atributo data igual: " + r1.equals(r2));
+	}
+	*/
+	
+	public static void main(String[] args) {
+		Resource r1 = new Resource();
+		r1.data = "ALPHA";
+		Resource r2 = new Resource();
+		System.out.println("Son iguales con atributo data igual: " + r1.equals(r2));
+	}
 
 }
