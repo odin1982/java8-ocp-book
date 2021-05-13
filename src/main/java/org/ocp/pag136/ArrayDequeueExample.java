cbdc1056217dd00f2b12b31d1c16199ff1911d7a
odinPcWindows <odin.araujo.barragan@gmail.com>
2021-05-13 13:48:56 -0500

    Queue - se mejoran ejercicios

--- src/main/java/org/ocp/pag136/ArrayDequeueExample.java
+++ src/main/java/org/ocp/pag136/ArrayDequeueExample.java
@@ -5,6 +5,7 @@ import java.util.Queue;
 
 public class ArrayDequeueExample {
 	public static void main(String[] args) {
+		/*
 		Queue<Integer> queue = new ArrayDeque<>();
 		System.out.println(queue.offer(10));
 		System.out.println(queue);
@@ -23,6 +24,17 @@ public class ArrayDequeueExample {
 		
 		System.out.println(queue.peek());
 		System.out.println(queue);
+		*/
+		
+		
+	
+		ArrayDeque<Integer> queue = new ArrayDeque<>();
+		queue.offer(1);System.out.println("queue: " + queue);//offer agrega elemento a la cola
+		queue.offer(2);System.out.println("queue: " + queue);
+		queue.offer(3);System.out.println("queue: " + queue);
+		queue.offer(4);System.out.println("queue: " + queue);
+		System.out.println("regresa el siguiente elemento de la cola: " + queue.peek());System.out.println("queue: " + queue);
+		System.out.println("elimina y regresa el siguiente elemento: " + queue.poll());System.out.println("queue: " + queue);
 	}
 
 }
--- src/main/java/org/ocp/pag137/ArrayDequeLikeStackExample.java
+++ src/main/java/org/ocp/pag137/ArrayDequeLikeStackExample.java
@@ -5,12 +5,15 @@ import java.util.ArrayDeque;
 public class ArrayDequeLikeStackExample {
 	public static void main(String[] args) {
 		ArrayDeque<Integer> stack = new ArrayDeque<>();
-		stack.push(10);
-		stack.push(4);
-		System.out.println(stack.peek());
-		System.out.println(stack.poll());
-		System.out.println(stack.poll());
-		System.out.println(stack.peek());
+		stack.push(1);System.out.println(stack);
+		stack.push(2);System.out.println(stack);
+		stack.push(3);System.out.println(stack);
+		stack.push(4);System.out.println(stack);
+		stack.push(5);System.out.println(stack);
+		stack.push(6);System.out.println(stack);
+		System.out.print("Retorna siguiente elemento: " + stack.peek() + "    -   ");System.out.println(stack);
+		System.out.print("Remeve el siguiente elemento: " + stack.poll() + "    -   ");System.out.println(stack);
+		System.out.print("Remeve el siguiente elemento: " + stack.poll() + "    -   ");System.out.println(stack);
 	}
 
 }
