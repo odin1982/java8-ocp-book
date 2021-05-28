4da7dc1eb7a12a7e34c502245286a1ef4cccde17
odin <odin@joker>
2021-05-28 07:30:07 -0500

    Stream - flatMap()

--- src/main/java/org/ocp/pag198/FlatMapStreamExample.java
+++ src/main/java/org/ocp/pag198/FlatMapStreamExample.java
@@ -1,7 +1,18 @@
 package org.ocp.pag198;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class FlatMapStreamExample {
 	public static void main(String[] args) {
+		List<String> zero = Arrays.asList();
+		List<String> one = Arrays.asList("Bonobo");
+		List<String> two = Arrays.asList("Mama Gorilla","Baby Gorilla");
+		Stream<List<String>> animals = Stream.of(zero,one,two);
 		
+		// As you can see, it removed the empty list completely and changed all elements 
+		// of each list to be at the top level of the stream
+		animals.flatMap(l -> l.stream()).forEach(System.out::println);
 	}
 }
