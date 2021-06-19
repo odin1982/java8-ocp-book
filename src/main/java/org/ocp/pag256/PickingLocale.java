ecbb88518a088235eb3cee56aef55829050b8a98
odinPcWindows <odin.araujo.barragan@gmail.com>
2021-06-19 14:19:52 -0500

    Resource Boundle

---
+++ src/main/java/org/ocp/pag256/PickingLocale.java
@@ -0,0 +1,11 @@
+package org.ocp.pag256;
+
+import java.util.Locale;
+
+public class PickingLocale {
+	public static void main(String[] args) {
+		Locale locale = Locale.getDefault();
+		System.out.println(locale);//es_MX
+	}
+
+}
