5c0122cb947a59ff632c42ed1528b4481d92bd39
odinLaptopUbuntu <odin.araujo.barragan@gmail.com>
2021-06-20 09:45:14 -0500

    Handling variables Inside Resource Bundles

--- src/main/java/org/ocp/pag266/ResourceBoundleFindExample.java
+++ src/main/java/org/ocp/pag266/ResourceBoundleFindExample.java
@@ -1,5 +1,6 @@
 package org.ocp.pag266;
 
+import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -14,5 +15,10 @@ public class ResourceBoundleFindExample {
 		System.out.print(rb.getString("open"));
 		System.out.print(" ");
 		System.out.print(rb.getString("visitor"));
+		
+		System.out.println();
+		String format = rb.getString("oso");
+		String formatted = MessageFormat.format(format, "Tammy");
+		System.out.println(formatted);
 	}
 }
