63dee874467b94c2ce7461fda9b853cabe2ede93
odin-huawei <odin.araujo.barragan@gmail.com>
2021-07-16 16:41:10 -0500

    Concurrent Collections

---
+++ src/main/java/org/ocp/pag358/ZooManager2.java
@@ -0,0 +1,17 @@
+package org.ocp.pag358;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ZooManager2 {
+	private Map<String,Object> foodData = new ConcurrentHashMap<String,Object>();
+	
+	public synchronized void put(String key,String value) {
+		foodData.put(key, value);
+	}      
+
+	public synchronized Object get(String key) {
+		return foodData.get(key);
+	}
+}
