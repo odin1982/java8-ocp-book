5ff7328bd65b04145ebba9958920b2096fbda326
odin-huawei <odin.araujo.barragan@gmail.com>
2021-08-05 23:35:35 -0500

    Se agrega test

---
+++ src/main/java/org/enthuware/javaClassDesign/_09/BankAccount.java
@@ -0,0 +1,18 @@
+package org.enthuware.javaClassDesign._09;
+
+interface Account {
+	public default String getId() {
+		return "0000";
+	}
+}
+
+interface PremiumAccount extends Account {
+	public String getId();
+}
+
+public class BankAccount implements PremiumAccount{
+	public static void main(String[] args) {
+		Account acct = new BankAccount();
+		System.out.println(acct.getId());
+	}
+}
